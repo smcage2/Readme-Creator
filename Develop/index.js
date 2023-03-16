eb8a6846772ea749bafaec10d9e5f1f788564dcc
smcage2 <smcage2@gmail.com>
2023-03-16 11:10:17 -0500

    updated javascript

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,70 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown').default.default;
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+        {
+          type: 'input',
+          message: 'Please enter the title of your project: ',
+          name: 'title',
+        },
+        {
+          type: 'input',
+          message: 'Please provide a brief description of your project: ',
+          name: 'description',
+        },
+        {
+          type: 'input',
+          message: 'Please provide the steps required to install your project: ',
+          name: 'installation',
+        },
+        {
+          type: 'input',
+          message: 'Please provide your project usage: ',
+          name: 'usage',
+        },
+        {
+          type: 'input',
+          message: 'Please list anyone you like to give credit to for helping to influnence this project: ',
+          name: 'credits',
+        },
+        {
+          type: 'choices',
+          message: 'Select the  license used for your project: ',
+          name: 'license',
+          selection: ['GNU GPL v3', 'MIT', 'None'],
+        },
+        {
+          type: 'input',
+          message: 'Please enter your GitHub username: ',
+          name: 'github',
+        },
+        {
+          type: 'input',
+          message: 'Please provide your email address: ',
+          name: 'email',
+        },
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err)
+            throw err;
+        console.log('Your README.md was created successfully.')
+    });
+  };
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function (userInput) {
+        writeToFile("README.md", generateMarkdown(userInput));
+    });
+  }
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,94 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license == "MIT"){
+      return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+    } else {
+      return 
+    }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license == "MIT"){
+      return `(https://opensource.org/licenses/MIT)`
+    } else {
+      return 
+    }
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "MIT"){
+    return `
+    MIT License ${data.github}
+    
+    Copyright (c) 2022 
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+    `
+  }
+    else {
+    return 
+  }
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  
+  # ${data.title}   ${renderLicenseBadge(data.license)}
 
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+    * [Description] (#description)
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [Credits](#credits)
+    * [License](#license)
+    * [Questions](#questions)
+      
+  ## Installation
+  ${data.installation} 
+
+  ## Usage 
+  ${data.usage}
+
+  ## Credits 
+  ${data.credits}
+
+  ## License
+  ${renderLicenseLink(data.license)}
+  ${renderLicenseSection(data.license)}
+
+
+  ## Questions
+  If you have any questions please feel free to contact me at:
+  - Github:(https://github.com/${data.github})  
+  - Email: ${data.email}
 `;
 }
 
-module.exports = generateMarkdown;
+export default generateMarkdown;
